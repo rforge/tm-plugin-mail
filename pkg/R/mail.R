0b475ed70078a36914b22d5da444237cb28e1469
khornik <khornik@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2018-06-11 16:59:38 +0000

    Tweaks for c8.
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/tm-plugin-mail@18 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/mail.R
+++ pkg/R/mail.R
@@ -124,8 +124,13 @@ threads <- function(x)
     threadIDs <- threadLevels <- integer(length(x))
     for (i in seq_along(x)) {
         messageID <- meta(x[[i]], "id")
-        parentID <- gsub("In-Reply-To: ", "", grep("^In-Reply-To:", attr(x[[i]], "Header"), value = TRUE, useBytes = TRUE))
-        refID <- gsub("References: ", "", grep("^References:", attr(x[[i]], "Header"), value = TRUE, useBytes = TRUE))
+        header <- meta(x[[i]], "header")
+        parentID <- gsub("In-Reply-To: ", "",
+                         grep("^In-Reply-To:", header,
+                              value = TRUE, useBytes = TRUE))
+        refID <- gsub("References: ", "",
+                      grep("^References:", header,
+                           value = TRUE, useBytes = TRUE))
         refID <- sub(",$", "", refID)
 
         # Generate new thread
