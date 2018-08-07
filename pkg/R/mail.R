c3595adef946a9846ce1b66dae9a8ff48ef4c843
khornik <khornik@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2018-08-07 19:45:22 +0000

    Have readMail() always parse email headers.
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/tm-plugin-mail@20 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/mail.R
+++ pkg/R/mail.R
@@ -1,19 +1,21 @@
-convert_mbox_eml <- function(mbox, dir) {
+convert_mbox_eml <-
+function(mbox, dir)
+{
     dir.create(dir, recursive = TRUE)
     content <- readLines(mbox)
-    counter <- start <- end <- 1
-    nMessages <- length(grep("^From ", content, useBytes = TRUE))
-    fmt <- paste("%0", nchar(nMessages), "d", sep = "")
+    counter <- start <- end <- 1L
+    nMessages <- sum(startsWith(content, "From "))
+    fmt <- paste0("%0", nchar(nMessages), "d")
     needWrite <- FALSE
     for (i in seq_along(content)) {
-        if (length(grep("^From ", content[i], useBytes = TRUE)) > 0) {
-            end <- i - 1
-            if (needWrite && start <= end) {
+        if (startsWith(content[i], "From ")) {
+            end <- i - 1L
+            if (needWrite && (start <= end)) {
                 con <- file(file.path(dir, sprintf(fmt, counter)))
-                writeLines(content[start:end], con)
+                writeLines(content[start : end], con)
                 close(con)
                 needWrite <- FALSE
-                counter <- counter + 1
+                counter <- counter + 1L
             }
             start <- i
             needWrite <- TRUE
@@ -21,126 +23,152 @@ convert_mbox_eml <- function(mbox, dir) {
     }
     if (needWrite) {
         con <- file(file.path(dir, sprintf(fmt, counter)))
-        writeLines(content[start:length(content)], con)
+        writeLines(content[start : length(content)], con)
         close(con)
     }
     invisible(TRUE)
 }
 
-# Remove e-mail citations beginning with >
-removeCitation <- function(x, removeQuoteHeader) UseMethod("removeCitation", x)
-removeCitation.character <- function(x, removeQuoteHeader = FALSE) {
+## Remove e-mail citations beginning with >
+removeCitation <-
+function(x, removeQuoteHeader)
+    UseMethod("removeCitation", x)
+
+removeCitation.character <-
+function(x, removeQuoteHeader = FALSE)
+{
     citations <- grep("^[[:blank:]]*>", x, useBytes = TRUE)
     if (removeQuoteHeader) {
-      headers <- grep("wrote:$|writes:$", x)
-      # A quotation header must immediately preceed a quoted part,
-      # possibly with one empty line in between
-      headers <- union(headers[(headers + 1) %in% citations],
-                       headers[(headers + 2) %in% citations])
-      citations <- union(headers, citations)
+        headers <- grep("wrote:$|writes:$", x)
+        ## A quotation header must immediately preceed a quoted part,
+        ## possibly with one empty line in between.
+        headers <- union(headers[(headers + 1L) %in% citations],
+                         headers[(headers + 2L) %in% citations])
+        citations <- union(headers, citations)
     }
     if (length(citations)) x[-citations] else x
 }
+
 removeCitation.MailDocument <-
     content_transformer(removeCitation.character)
 
-# Remove non-text parts from multipart e-mail messages
-removeMultipart <- function(x) UseMethod("removeMultipart", x)
-removeMultipart.character <- function(x) {
-    # http://en.wikipedia.org/wiki/Multipart_message#Multipart_Messages
-    # We are only interested in text/plain parts
+## Remove non-text parts from multipart e-mail messages
+removeMultipart <-
+function(x)
+    UseMethod("removeMultipart", x)
+
+removeMultipart.character <-
+function(x)
+{
+    ## <http://en.wikipedia.org/wiki/Multipart_message#Multipart_Messages>
+    ## We are only interested in text/plain parts
     i <- grep("^Content-Type: text/plain", x, useBytes = TRUE)
     r <- character(0)
-    k <- 2
+    k <- 2L
     for (j in i) {
-        end <- if (k <= length(i)) i[k]-1 else length(x)
-        content <- x[j:end]
+        end <- if (k <= length(i)) i[k] - 1L else length(x)
+        content <- x[j : end]
         ## Find boundary (starting with "--")
-        # In most cases the boundary is just one line before the Content-Type header
-        start <- j - 1
-        while (j > 0) {
-            if (substr(x[j], 1, 2) == "--") {
+        ## In most cases the boundary is just one line before the
+        ## Content-Type header.
+        start <- j - 1L
+        while (j > 0L) {
+            if (startsWith(x[j], "--")) {
                 start <- j
                 break
             }
             else
-                j <- j - 1
+                j <- j - 1L
         }
         index <- grep(x[start], content, useBytes = TRUE, fixed = TRUE)
-        index <- if (length(index) == 0) length(content) else (index[1] - 1)
-        content <- content[1:index]
-        # Now remove remaining headers
+        index <- if (length(index)) (index[1L] - 1L) else length(content)
+        content <- content[1L : index]
+        ## Now remove remaining headers
         index <- grep("^$", content, useBytes = TRUE)
-        index <- if (length(index) == 0) 1 else (index[1] + 1)
-        r <- c(r, content[index:length(content)])
-        k <- k + 1
+        index <- if (length(index)) (index[1L] + 1L) else 1L
+        r <- c(r, content[index : length(content)])
+        k <- k + 1L
     }
 
-    if (length(r) == 0) x else r
+    if (!length(r)) x else r
 }
+
 removeMultipart.MailDocument <-
     content_transformer(removeMultipart.character)
 
 # Remove e-mail signatures
-removeSignature <- function(x, marks) UseMethod("removeSignature", x)
-removeSignature.character <- function(x, marks = character(0)) {
-    # "---" is often added to Sourceforge mails
-    # "___" and "***" are also common, i.e.,
-    # marks <- c("^_{10,}", "^-{10,}", "^[*]{10,}")
+removeSignature <-
+function(x, marks)
+    UseMethod("removeSignature", x)
 
-    # "-- " is the official correct signature start mark
+removeSignature.character <-
+function(x, marks = character(0))
+{
+    ## "---" is often added to Sourceforge mails
+    ## "___" and "***" are also common, i.e.,
+    ## marks <- c("^_{10,}", "^-{10,}", "^[*]{10,}")
+
+    ## "-- " is the official correct signature start mark
     marks <- c("^-- $", marks)
 
-    signatureStart <- length(x) + 1
-    for (m in marks)
-        signatureStart <- min(grep(m, x, useBytes = TRUE), signatureStart)
+    n <- length(x)
 
-    if (signatureStart <= length(x)) x[-(signatureStart:length(x))]
-    else x
+    signatureStart <- n + 1L
+    for (m in marks)
+        signatureStart <-
+            min(grep(m, x, useBytes = TRUE), signatureStart)
+
+    if (signatureStart <= n)
+        x[-(signatureStart : n)]
+    else
+        x
 }
 removeSignature.MailDocument <-
     content_transformer(removeSignature.character)
 
-get.thread.id <- function(parentID, ht) {
-    threadID <- NA
-    threadLevel <- 2
+get.thread.id <-
+function(parentID, ht)
+{
+    threadID <- NA_character_
+    threadLevel <- 2L
 
-    if (!identical(parentID, "") && length(parentID) != 0 && is.numeric(ht[[parentID]][1]))
-        threadID <- as.integer(ht[[parentID]][1])
+    if (!identical(parentID, "")
+        && (length(parentID) != 0)
+        && is.numeric(id <- ht[[parentID]][1L]))
+        threadID <- as.integer(id)
 
-    if (!identical(parentID, "") && length(parentID) != 0 && is.numeric(ht[[parentID]][2]))
-        threadLevel <- as.integer(ht[[parentID]][2] + 1)
+    if (!identical(parentID, "")
+        && (length(parentID) != 0)
+        && is.numeric(id <- ht[[parentID]][2L]))
+        threadLevel <- as.integer(id + 1L)
 
     list(threadID = threadID, threadLevel = threadLevel)
 }
 
-# Compute thread IDs and thread depth (based on heuristics)
-# See http://www.jwz.org/doc/threading.html for a more complete and correct approach
+## Compute thread IDs and thread depth (based on heuristics).
+## See <http://www.jwz.org/doc/threading.html> for a more complete and
+## correct approach.
 threads <- function(x)
 {
-    # Hash table storing (thread ID, thread level) for a given message ID
+    ## Hash table storing (thread ID, thread level) for a given message
+    ## ID.
     ht <- new.env()
-    tid <- 1
+    tid <- 1L
     threadIDs <- threadLevels <- integer(length(x))
     for (i in seq_along(x)) {
         messageID <- meta(x[[i]], "id")
         header <- meta(x[[i]], "header")
-        parentID <- gsub("In-Reply-To: ", "",
-                         grep("^In-Reply-To:", header,
-                              value = TRUE, useBytes = TRUE))
-        refID <- gsub("References: ", "",
-                      grep("^References:", header,
-                           value = TRUE, useBytes = TRUE))
-        refID <- sub(",$", "", refID)
+        parentID <- header$"In-Reply-To"
+        refID <- sub(">.*", ">", header$"References")
 
-        # Generate new thread
-        if (!length(parentID) & !length(refID)) {
-            ht[[messageID]] <- c(tid, 1)
+        ## Generate new thread
+        if (!length(parentID) && !length(refID)) {
+            ht[[messageID]] <- c(tid, 1L)
             threadIDs[i] <- tid
-            threadLevels[i] <- 1
-            tid <- tid + 1
+            threadLevels[i] <- 1L
+            tid <- tid + 1L
         }
-        # Use existing thread
+        ## Use existing thread
         else {
             info <- get.thread.id(refID, ht)
             if (!is.na(info$threadID)) {
@@ -151,7 +179,7 @@ threads <- function(x)
             }
 
             parentID <- unique(parentID)
-            if (length(parentID) > 1) {
+            if (length(parentID) > 1L) {
                 for (i in 1:length(parentID)) {
                     info <- get.thread.id(parentID[[i]], ht)
                     if (!is.na(info$threadID))
@@ -161,13 +189,14 @@ threads <- function(x)
                 info <- get.thread.id(parentID, ht)
 
             if (is.na(info$threadID)) {
-                # The message is a reply to some other message, but the parent
-                # e-mail is not available --- for instance, it could be a follow-up
-                # to a discussion from a separate mailing list. Create a new thread.
-                ht[[messageID]] <- c(tid, 1)
+                ## The message is a reply to some other message, but the
+                ## parent e-mail is not available --- for instance, it
+                ## could be a follow-up to a discussion from a separate
+                ## mailing list.  Create a new thread.
+                ht[[messageID]] <- c(tid, 1L)
                 threadIDs[i] <- tid
-                threadLevels[i] <- 1
-                tid <- tid + 1
+                threadLevels[i] <- 1L
+                tid <- tid + 1L
             }
             else {
                 ht[[messageID]] <- c(info$threadID, info$threadLevel)
--- pkg/R/reader.R
+++ pkg/R/reader.R
@@ -10,28 +10,49 @@ function(DateFormat = "%d %B %Y %H:%M:%S")
         mail <- elem$content
 
         ## The header is separated from the body by a blank line.
-        ## http://en.wikipedia.org/wiki/E-mail#Internet_e-mail_format
-        for (index in seq_along(mail))
-            if (identical(mail[index], "")) break
+        ## <http://en.wikipedia.org/wiki/E-mail#Internet_e-mail_format>
+        index <- Position(function(e) e == "", mail)
 
-        header <- mail[1:index]
-        content <- mail[(index + 1):length(mail)]
+        header <- mail[1L : (index - 1L)]
+        content <- mail[(index + 1L) : length(mail)]
 
-        author <- gsub("From: ", "",
-                       grep("^From:", header, value = TRUE, useBytes = TRUE))
+        ## Parse message header.
+        ## See RFC 5322 <https://tools.ietf.org/html/rfc5322>.
+        ## Section 2.2 of RFC 5322 says that
+        ##   Header fields are lines beginning with a field name,
+        ##   followed by a colon (":"), followed by a field body, and
+        ##   terminated by CRLF.  A field name MUST be composed of
+        ##   printable US-ASCII characters (i.e., characters that have
+        ##   values between 33 and 126, inclusive), except colon.
+        ## For convenience, let us also trim leading spaces in the field
+        ## values.
+
+        ## Note that RFC 5322 Section 3.6 gives the limits on the min
+        ## and max numbers of times fields may occur in the header, and
+        ## there are fields which may occur arbitrarily often.  So we
+        ## must parse into a *list*.
+
+        pat <- "^([\041-\071\073-\176]+): *(.*)"
+        ind <- grepl(pat, header, perl = TRUE)
+        header <- split(header, cumsum(ind))
+        header <- unlist(lapply(header, paste, collapse = ""),
+                         use.names = FALSE)
+        header <- split(sub(pat, "\\2", header, perl = TRUE),
+                        sub(pat, "\\1", header, perl = TRUE))
+
         datetimestamp <-
-            strptime(gsub("Date: ", "",
-                          grep("^Date:", header, value = TRUE, useBytes = TRUE)),
-                     format = format,
-                     tz = "GMT")
-        mid <- gsub("Message-ID: ", "",
-                    grep("^Message-ID:", header, value = TRUE, useBytes = TRUE))
-        origin <- gsub("Newsgroups: ", "",
-                       grep("^Newsgroups:", header, value = TRUE, useBytes = TRUE))
-        heading <- gsub("Subject: ", "",
-                        grep("^Subject:", header, value = TRUE, useBytes = TRUE))
+            strptime(header$Date,
+                     format = format, tz = "GMT")
+        mid <- header$"Message-ID"
 
-        MailDocument(content, author, datetimestamp, character(0), header, heading,
-                     if (length(mid)) mid[1] else id, language, origin)
+        MailDocument(content,
+                     header$From,
+                     datetimestamp,
+                     character(0),
+                     header,
+                     header$Subject,
+                     if (length(mid)) mid[1L] else id,
+                     language,
+                     header$Newsgroups)
     }
 }, class = c("FunctionGenerator", "function"))
