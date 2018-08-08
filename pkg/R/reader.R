4866e3b98dc9cd479563a34ac1560e8ac8096794
khornik <khornik@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2018-08-08 05:43:47 +0000

    Improve handling of RFC 5322 date-times.
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/tm-plugin-mail@23 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/reader.R
+++ pkg/R/reader.R
@@ -1,11 +1,10 @@
 ## Author: Ingo Feinerer
 
 readMail <- structure(
-function(DateFormat = "%d %B %Y %H:%M:%S")
+function(DateFormat = character())
 {
     stopifnot(is.character(DateFormat))
 
-    format <- DateFormat
     function(elem, language, id) {
         mail <- elem$content
 
@@ -16,39 +15,16 @@ function(DateFormat = "%d %B %Y %H:%M:%S")
         header <- mail[1L : (index - 1L)]
         content <- mail[(index + 1L) : length(mail)]
 
-        ## Parse message header.
-        ## See RFC 5322 <https://tools.ietf.org/html/rfc5322>.
-        ## Section 2.2 of RFC 5322 says that
-        ##   Header fields are lines beginning with a field name,
-        ##   followed by a colon (":"), followed by a field body, and
-        ##   terminated by CRLF.  A field name MUST be composed of
-        ##   printable US-ASCII characters (i.e., characters that have
-        ##   values between 33 and 126, inclusive), except colon.
-        ## For convenience, let us also trim leading spaces in the field
-        ## values.
-
-        ## Note that RFC 5322 Section 3.6 gives the limits on the min
-        ## and max numbers of times fields may occur in the header, and
-        ## there are fields which may occur arbitrarily often.  So we
-        ## must parse into a *list*.
-
-        pat <- "^([\041-\071\073-\176]+): *(.*)"
-        ind <- grepl(pat, header, perl = TRUE)
-        header <- split(header, cumsum(ind))
-        header <- unlist(lapply(header, paste, collapse = ""),
-                         use.names = FALSE)
-        header <- split(sub(pat, "\\2", header, perl = TRUE),
-                        sub(pat, "\\1", header, perl = TRUE))
+        header <- parse_RFC_5322_message_header(header)
 
         datetimestamp <-
-            strptime(header$Date,
-                     format = format, tz = "GMT")
+            parse_RFC_5322_date_time(header$Date, DateFormat)
         mid <- header$"Message-ID"
 
         MailDocument(content,
                      header$From,
                      datetimestamp,
-                     character(0),
+                     character(),
                      header,
                      header$Subject,
                      if (length(mid)) mid[1L] else id,
@@ -56,3 +32,58 @@ function(DateFormat = "%d %B %Y %H:%M:%S")
                      header$Newsgroups)
     }
 }, class = c("FunctionGenerator", "function"))
+
+parse_RFC_5322_message_header <-
+function(x)
+{
+    ## Parse message header.
+    ## See RFC 5322 <https://tools.ietf.org/html/rfc5322>.
+    ## Section 2.2 of RFC 5322 says that
+    ##   Header fields are lines beginning with a field name, followed
+    ##   by a colon (":"), followed by a field body, and terminated by
+    ##   CRLF.  A field name MUST be composed of printable US-ASCII
+    ##   characters (i.e., characters that have values between 33 and
+    ##   126, inclusive), except colon. 
+    ## For convenience, let us also trim leading spaces in the field
+    ## values.
+    
+    ## Note that RFC 5322 Section 3.6 gives the limits on the min and
+    ## max numbers of times fields may occur in the header, and there
+    ## are fields which may occur arbitrarily often.  So we must parse
+    ## into a *list*.
+    
+    p <- "^([\041-\071\073-\176]+): *(.*)"
+    i <- grepl(p, x, perl = TRUE)
+    y <- split(x, cumsum(i))
+    ## Unfold.
+    y <- unlist(lapply(y, paste, collapse = ""), use.names = FALSE)
+    split(sub(p, "\\2", y, perl = TRUE),
+          sub(p, "\\1", y, perl = TRUE))
+}
+
+parse_RFC_5322_date_time <-
+function(x, format = character())
+{
+    ## "Basic" formats, see <https://tools.ietf.org/html/rfc5322>,
+    ## Section "3.3.  Date and Time Specification".
+    if(!length(format))
+        format <- c("%a, %d %B %Y %H:%M:%S %z",
+                    "%d %B %Y %H:%M:%S %z")
+    ## However, the abbreviated weekday names are in English, so for %a
+    ## we must use a C locale:
+    lc <- Sys.getlocale("LC_TIME")
+    Sys.setlocale("LC_TIME", "C")
+    on.exit(Sys.setlocale("LC_TIME", lc))
+
+    y <- strptime(x, format[1L], tz = "GMT")
+    i <- which(is.na(y))
+    if(length(i)) {
+        for(f in format[-1L]) {
+            y[i] <- strptime(x[i], f, tz = "GMT")
+            i <- which(is.na(y))
+            if(!length(i)) break
+        }
+    }
+
+    y
+}
